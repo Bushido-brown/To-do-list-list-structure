ac30d0e070ce03e7c135cbdca5d975089416c305
Bushido-brown <orizut@gmail.com>
2022-06-10 15:44:54 -0700

    add unit test for add function

--- src/list_functions.test.js
+++ src/list_functions.test.js
@@ -13,22 +13,3 @@ describe('add functions method', ()=>{
 
   })
 })
-
-
-describe('erase functions method', ()=>{
-  
-  it('this should be able to remove a specific task from the array', ()=>{
-    const arr =[
-      {description: 'first text', completed: false,index: 0,},
-      {description: 'second text', completed: false,index: 1,},
-      {description: 'third text', completed: false,index: 2,},
-  ];
-    erase(arr, 1)
-    erase(arr, 2)
-    erase(arr, 0)
-
-
-    expect(arr.length).toBe(0)
-
-  })
-})
